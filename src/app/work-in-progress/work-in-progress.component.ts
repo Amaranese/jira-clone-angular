54c6c2867a81341b44d438a2dfdbdf694d3de672
Amaranese <alejandromaranes@gmail.com>
2022-07-11 12:50:15 +0200

    -

---
+++ src/app/work-in-progress/work-in-progress.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'work-in-progress',
+  templateUrl: './work-in-progress.component.html',
+  styleUrls: ['./work-in-progress.component.scss']
+})
+export class WorkInProgressComponent {
+
+  constructor() { }
+
+}
